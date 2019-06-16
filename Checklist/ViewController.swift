61d0e63bbf9543b73f43a818b2042151a71b967a
Johnathan Tam <>
2019-06-16 10:23:54 -0600

    Initial setup.

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Johnathan Tam on 2019-06-16.
+//  Copyright © 2019 John's Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
