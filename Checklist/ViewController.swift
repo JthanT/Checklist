7228c0509ec886b8fffad430ac0b0e6e2d6c59db
Johnathan Tam <>
2019-06-17 11:07:38 -0600

    Added task entry and persistence data storage and retrieval.
    Adding new tasks wil save the entry into Core Data. The table view also fetches for previously entered tasks as well.

--- Checklist/ViewController.swift
+++ Checklist/ViewController.swift
@@ -21,12 +21,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let displayWidth: CGFloat = self.view.frame.width
         let displayHeight: CGFloat = self.view.frame.height
         
-        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight - barHeight))
-        tableView.register(ChecklistHeader.self, forHeaderFooterViewReuseIdentifier: "headerId")
-        tableView.sectionHeaderHeight = self.view.bounds.height * 0.1
-        tableView.register(TaskCell.self, forCellReuseIdentifier: "MyCell")
-        tableView.dataSource = self
-        tableView.delegate = self
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight - barHeight))
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        do {
+            let tasks = try PersistenceService.context.fetch(fetchRequest)
+            self.tasks = tasks
+            self.tableView.reloadData()
+        } catch {
+            
+        }
+        
+        
+        self.tableView.register(ChecklistHeader.self, forHeaderFooterViewReuseIdentifier: "headerId")
+        self.tableView.sectionHeaderHeight = self.view.bounds.height * 0.1
+        self.tableView.register(TaskCell.self, forCellReuseIdentifier: "MyCell")
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
         self.view.addSubview(tableView)
     }
     
@@ -38,14 +50,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
+        return self.tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath) as! TaskCell
-        cell.taskItemLabel.text = tasks[indexPath.row].task
+        cell.taskItemLabel.text = self.tasks[indexPath.row].task
         
         return cell
+    }
+    
+    func addChecklistItem(checklistItem: String) {
+        let taskItem = Task(context: PersistenceService.context)
+        taskItem.task = checklistItem
+        PersistenceService.saveContext()
+        self.tasks.append(taskItem)
+        self.tableView?.reloadData()
     }
 }
 
@@ -80,21 +100,20 @@ class ChecklistHeader: UITableViewHeaderFooterView {
     }
 
     func setupViews() {
-        taskEntryTextField.topAnchor.constraint(equalToSystemSpacingBelow: self.contentView.topAnchor, multiplier: 1).isActive = true
-        taskEntryTextField.leftAnchor.constraint(equalToSystemSpacingAfter: self.contentView.leftAnchor, multiplier: 1.3).isActive = true
-        taskEntryTextField.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, multiplier: 0.8).isActive = true
-        taskEntryTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 3/4).isActive = true
+        self.taskEntryTextField.topAnchor.constraint(equalToSystemSpacingBelow: self.contentView.topAnchor, multiplier: 1).isActive = true
+        self.taskEntryTextField.leftAnchor.constraint(equalToSystemSpacingAfter: self.contentView.leftAnchor, multiplier: 1.3).isActive = true
+        self.taskEntryTextField.heightAnchor.constraint(equalTo: self.contentView.heightAnchor, multiplier: 0.8).isActive = true
+        self.taskEntryTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 3/4).isActive = true
 
-        taskEntryButton.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
-        taskEntryButton.leftAnchor.constraint(equalToSystemSpacingAfter: taskEntryTextField.rightAnchor, multiplier: 3).isActive = true
-        taskEntryButton.addTarget(self, action: #selector(addListEntry), for: .touchUpInside)
+        self.taskEntryButton.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+        self.taskEntryButton.leftAnchor.constraint(equalToSystemSpacingAfter: self.taskEntryTextField.rightAnchor, multiplier: 3).isActive = true
+        self.taskEntryButton.addTarget(self, action: #selector(addListEntry), for: .touchUpInside)
     }
 
     @objc func addListEntry() {
-//        viewController?.addChecklistItem(checklistItem: taskEntryTextField.text!)
-//        taskEntryTextField.text = ""
+        self.viewController?.addChecklistItem(checklistItem: self.taskEntryTextField.text!)
+        self.taskEntryTextField.text = ""
     }
-
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -106,8 +125,8 @@ class TaskCell: UITableViewCell {
     let taskItemLabel: UILabel = {
         let label = UILabel()
         label.text = ""
-        label.textColor = UIColor.white
-        label.backgroundColor = UIColor.black
+        label.textColor = UIColor.black
+        label.backgroundColor = UIColor.white
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -115,16 +134,17 @@ class TaskCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        addSubview(taskItemLabel)
+        addSubview(self.taskItemLabel)
         setupViews()
         
         self.selectionStyle = UITableViewCell.SelectionStyle.none
     }
     
     func setupViews() {
-        taskItemLabel.topAnchor.constraint(equalToSystemSpacingBelow: self.topAnchor, multiplier: 1.5).isActive = true
-        taskItemLabel.leftAnchor.constraint(equalToSystemSpacingAfter: self.leftAnchor, multiplier: 3).isActive = true
-        taskItemLabel.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        self.taskItemLabel.topAnchor.constraint(equalToSystemSpacingBelow: self.contentView.topAnchor, multiplier: 1.5).isActive = true
+//        taskItemLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor).isActive = true
+        self.taskItemLabel.leftAnchor.constraint(equalToSystemSpacingAfter: self.contentView.leftAnchor, multiplier: 1.5).isActive = true
+        self.taskItemLabel.widthAnchor.constraint(equalTo: self.contentView.widthAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
